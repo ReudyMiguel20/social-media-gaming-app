75cc01046963cdb46702ca92f223e1c32cd9a858
mike <reudymiguel20@gmail.com>
2025-01-31 16:29:42 -0400

    Modification to Post Entity and added PostStatus Enum

--- src/main/java/org/socialmedia/socialmediaapp/post/model/entity/Post.java
+++ src/main/java/org/socialmedia/socialmediaapp/post/model/entity/Post.java
@@ -1,15 +1,20 @@
 package org.socialmedia.socialmediaapp.post.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.URL;
+import org.socialmedia.socialmediaapp.post.model.enums.PostStatus;
 import org.socialmedia.socialmediaapp.post.model.enums.PostType;
 import org.socialmedia.socialmediaapp.user.model.entity.User;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "posts")
@@ -23,19 +28,46 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 2, max = 100)
     private String title;
 
+    @Size(max = 1000)
     private String content;
 
-    private String url;
+//    @URL
+//    private String url;
+
+    // For Photo or Video in the post
+    private String mediaUrl;
 
     @Enumerated(EnumType.STRING)
     private PostType postType;
 
+    @Enumerated(EnumType.STRING)
+    private PostStatus postStatus;
+
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    @ManyToOne
+    private int likeCount;
+
+    @ElementCollection
+    @Builder.Default
+    private Set<String> tags = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", mediaUrl='" + mediaUrl + '\'' +
+                ", postType=" + postType +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 
 }
